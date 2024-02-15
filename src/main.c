fbb13c69da769caff591851ca10b9aea65228803
Kurama77190 <samybentayeb@outlook.com>
2024-02-16 00:20:31 +0100

    implement note in main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 15:46:19 by sben-tay          #+#    #+#             */
-/*   Updated: 2024/02/15 23:59:33 by sben-tay         ###   ########.fr       */
+/*   Updated: 2024/02/16 00:19:26 by sben-tay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,29 @@ int main(int argc, char **argv)
 /*
 	- PARSING SECURITY 100% OK
 	- NOW, I NEED IMPLEMENT ALGORITHM ! LETSGOOO
+*/
+
+/* *************************** */
+/* 	 	  COMMANDS TEST	 	   */
+/* *************************** */
+
+/*
+	- check error arguments : 
+
+	./push_swap
+	./push_swap "" "9 8 7"
+	./push_swap "    "
+	./push_swap ""
+	./push_swap 9 8 7 ""
+	./push_swap "abcd"
+	./push_swap "9 9 8 7 6"
+	./push_swap "2147483648 2 3 4"
+	./push_swap "9223372036854775808 2 3 4"
+
+	- check valid arguments :
+	
+	./push_swap "9 8 7 
+	6"
+	./push_swap "  " 9 8 7 6 "  "
+	./push_swap 100 1 2 3 4""5 6
 */
